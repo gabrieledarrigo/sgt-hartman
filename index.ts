827fd79960481425c342eb443e0e3b733fdb7a8c
Gabriele D'Arrigo+ <darrigo.g@gmail.com>
2025-04-30 16:43:58 +0200

    Read the exercises file

--- index.ts
+++ index.ts
@@ -1,51 +1,66 @@
 import { readFile } from 'fs/promises';
+import { join } from 'path';
 
-type Exercise = {
+type Esercizio = {
   nome: string;
+  categorie: string[];
   varianti: string[];
+  attrezzatura?: string[];
+  tutorial?: {
+    [key: string]: string;
+  };
+}
+
+type Categorie = {
+  parte_superiore: string[];
+  braccia: string[];
+  parte_inferiore: string[];
+  core: string[];
+  movimento: string[];
+  tipo: string[];
+}
+
+type FormatoAllenamento = {
+  tipo?: string;
+  formato?: string;
+  esempio?: string;
+  formati?: string[];
 }
 
 type Video = {
-  descrizione: string;
-  url: string;
+  descrizione?: string;
+  url?: string;
+  durata?: string;
+  parte?: string;
+  descrizione_esercizi?: string;
+  tipo?: string;
 }
 
-type Esercises = {
-  esercizi: {
-    parte_superiore: {
-      petto: Exercise;
-      schiena: Exercise;
-      spalle: Exercise;
-      braccia: {
-        tricipiti: Exercise;
-        bicipiti: Exercise;
-      };
-    };
-    parte_inferiore: {
-      quadricipiti: Exercise;
-      femorali: Exercise;
-      glutei: Exercise;
-      polpacci: Exercise;
-    };
-    core: {
-      addominali: Exercise;
-      lombari: Exercise;
-    };
-    cardio: Exercise;
-    combinati: Exercise;
-    stretching_mobilita: Exercise;
-  };
-  video: Video;
+type Esercizi = {
+  esercizi: Esercizio[];
+  categorie: Categorie;
   attrezzi: string[];
-  formato_allenamento: {
-    tipo?: string;
-    formato?: string;
-    esempio?: string;
-    formati?: string[];
-  }[];
+  formato_allenamento: FormatoAllenamento[];
+  video_stretching?: Video[];
+  video_riscaldamento?: Video[];
+  video_allenamento?: Video[];
+  video_equilibrio?: Video[];
+  video_mobilita?: Video[];
 }
 
-function getExercises() {
+const EXERCISES_FILE_PATH = join(__dirname, './data/esercizi.json');
 
+async function getExercises(): Promise<Esercizi> {
+  const exercises = await readFile(EXERCISES_FILE_PATH, 'utf8');
+
+  return JSON.parse(exercises);
 }
 
+async function main() {
+  const exercises = await getExercises();
+
+  console.log(exercises);
+}
+
+await main()
+  .catch(err => console.error(err));
