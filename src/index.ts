d1fbaddc4a3d88b216a85bf465798d35e70a44a0
Gabriele D'Arrigo <darrigo.g@gmail.com>
2025-05-10 10:29:28 +0200

    Move the functions to generate the training inside the lib folder

--- src/index.ts
+++ src/index.ts
@@ -1,136 +1,36 @@
 import Groq from 'groq-sdk';
-import { format } from 'date-fns';
-import { it } from 'date-fns/locale';
-import type { Exercise, Training, TrainingLevel, Equipment } from './types';
-import { db } from "./db/db";
+import { db } from './db/db';
+import {
+  getExercises,
+  getEquipments,
+  getLastTrainings,
+  generateTraining,
+} from './lib';
+import type { Training } from './types';
 
 const GROQ_API_KEY = Bun.env.GROQ_API_KEY;
 const LLM_MODEL = Bun.env.LLM_MODEL;
 
 const groq = new Groq({ apiKey: GROQ_API_KEY });
 
-async function generateTraining(
-  exercises: Exercise[],
-  equipments: string[],
-  lastTrainings: Training[],
-  trainingLevel: TrainingLevel = 'intermedio',
-  desiredDuration: number = 60,
-  notes?: string,
-): Promise<string> {
-  const completion = await groq.chat.completions.create({
-    messages: [
-      {
-        role: 'system',
-        content: `
-          Sei un personal trainer esperto, specializzato nell'allenamento a corpo libero.
-          Il tuo compito è generare un allenamento a corpo libero partendo da un database di esercizi in formato JSON che ti verrà allegato nel corpo di ogni richiesta.
-          Oltre al database di esercizi, ti verranno forniti:
-          
-          - Il livello di difficoltà dell'allenamento e la durata desiderata in minuti.
-          - Alcuni allenamenti recenti, così che l'allenamento generato tenga conto del pregresso dell'utente che alleni.
-          - L'attrezzatura che puoi utilizzare per creare l'allenamento.
-          - Opzionalmente, alcune note scritte dall'utente con richieste specifiche o preferenze riguardo all'allenamento.
-        `,
-      },
-      {
-        role: 'user',
-        content: `
-          Genera un allenamento per oggi: ${format(new Date(), 'EEEE dd LLLL yyyy', { locale: it })} basato sui miei dati storici.
-
-          DATI UTENTE:
-          - Livello: ${trainingLevel}
-          - Durata desiderata: ${desiredDuration} minuti
-          - Note aggiuntive o preferenze: ${notes ? notes : 'Nessuna nota fornita dall utente.'}
-
-          DATABASE ESERCIZI:
-          ${JSON.stringify(exercises)}
-
-          ATTREZZATURA:
-          ${equipments.join(',')}
-
-          ALLENAMENTI RECENTI:
-          ${lastTrainings.map(training => training.training).join('\n\n')}
-
-          Crea un allenamento completo composto da:
-
-          - Una fase di riscaldamento, opzionale
-          - Una parte principale composta da una serie di circuiti 
-          - Una fase di stretching finale, opzionale
-          - Una breve descrizione dell'allenamento composta al massimo da una frase, opzionale.
-
-          Per i circuiti indica serie, ripetizioni (o tempo di esecuzione in secondi se l'esercizio non prevede ripetizioni), ed eventuali tempi di recupero per ogni esercizio.
-          Ometti le parti indicate come opzionali se non sono funzionali all'allenamento.
-          L'allenamento deve variare e non deve ripetere esattamente gli allenamenti precedenti: dev'essere studiato per aiutarmi a migliorare le mie prestazioni sulla base degli ultimi allenamenti.
-          L'allenamento deve rispettare il seguente formato Markdown:
-
-         **Allenamento [data in in italiano in formato EEEE dd LLLL yyyy]**
-          - [Riscaldamento] (opzionale) (link a un video tutorial, opzionale)
-          - **C1x[N]**: [ripetizioni o tempo in secondi] [Esercizio 1], [ripetizioni o tempo in secondi] [Esercizio 2], [ripetizioni o tempo in secondi] [Esercizio N]
-          - **C2x[N]**: [ripetizioni o tempo in secondi] [Esercizio 1], [ripetizioni o tempo in secondi] [Esercizio 2], [ripetizioni o tempo in secondi] [Esercizio N]
-          - **CNx[N]**: [ripetizioni o tempo in secondi] [Esercizio 1], [ripetizioni o tempo in secondi] [Esercizio 2], [ripetizioni o tempo in secondi] [Esercizio N]
-          - [Stretching] (opzionale)
-
-          ### Descrizione (opzionale)
-          - [Dettaglio descrizione]
-
-          Non aggiungere note o spiegazioni aggiuntive.
-        `,
-      },
-    ],
-    model: LLM_MODEL,
-  });
-
-  return (
-    completion.choices[0]?.message?.content ||
-    'Impossibile generare un esercizio.'
-  );
-}
-
-function getExercises(): Exercise[] {
-  const query = db.prepare<Exercise, {}>(`
-    SELECT id, name, categories, variations, equipments
-    FROM exercises;
-  `);
-
-  return query.all({}).map((exercise) => ({
-    ...exercise,
-    categories: JSON.parse(exercise.categories),
-    variations: JSON.parse(exercise.variations),
-    equipments: JSON.parse(exercise.equipments),
-  }));
-}
-
-function getEquipments(): string[] {
-  const query = db.query<Equipment, {}>(`
-    SELECT type
-    FROM equipments;
-  `);
-
-  return query.all({}).map((equipment) => equipment.type);
-}
-
-function getLastTrainings(numberOfTraining: number = 3): Training[] {
-  const query = db.prepare<Training, number>(`
-    SELECT *
-    FROM trainings
-    ORDER BY date DESC
-    LIMIT :numberOfTraining;
-  `);
-
-  return query.all(numberOfTraining);
-}
-
-async function main() {
+async function main(): Promise<Training> {
   const exercises = getExercises();
   const equipments = getEquipments();
   const lastTrainings = getLastTrainings();
-  const training = await generateTraining(exercises, equipments, lastTrainings);
+  const training = await generateTraining(
+    groq,
+    LLM_MODEL,
+    exercises,
+    equipments,
+    lastTrainings,
+  );
 
-  console.log(training);
+  return training;
 }
 
 await main()
+  .then((training) => console.log(training.training))
   .catch((err) => console.error(err))
   .finally(() => {
-    db.close(true)
+    db.close(true);
   });
---
+++ src/lib.ts
@@ -0,0 +1,122 @@
+import { format } from 'date-fns';
+import { it } from 'date-fns/locale';
+import { db } from './db/db';
+import type { Exercise, Training, TrainingLevel, Equipment } from './types';
+import type Groq from 'groq-sdk';
+
+export function getExercises(): Exercise[] {
+  const query = db.prepare<Exercise, {}>(`
+    SELECT id, name, categories, variations, equipments
+    FROM exercises;
+  `);
+
+  return query.all({}).map((exercise) => ({
+    ...exercise,
+    categories: JSON.parse(exercise.categories),
+    variations: JSON.parse(exercise.variations),
+    equipments: JSON.parse(exercise.equipments),
+  }));
+}
+
+export function getEquipments(): string[] {
+  const query = db.query<Equipment, {}>(`
+    SELECT type
+    FROM equipments;
+  `);
+
+  return query.all({}).map((equipment) => equipment.type);
+}
+
+export function getLastTrainings(numberOfTraining: number = 3): Training[] {
+  const query = db.prepare<Training, number>(`
+    SELECT *
+    FROM trainings
+    ORDER BY date DESC
+    LIMIT :numberOfTraining;
+  `);
+
+  return query.all(numberOfTraining);
+}
+
+export async function generateTraining(
+  groq: Groq,
+  model: string,
+  exercises: Exercise[],
+  equipments: string[],
+  lastTrainings: Training[],
+  trainingLevel: TrainingLevel = 'intermedio',
+  desiredDuration: number = 60,
+  notes?: string,
+): Promise<Training> {
+  const completion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `
+          Sei un personal trainer esperto, specializzato nell'allenamento a corpo libero.
+          Il tuo compito è generare un allenamento a corpo libero partendo da un database di esercizi in formato JSON che ti verrà allegato nel corpo di ogni richiesta.
+          Oltre al database di esercizi, ti verranno forniti:
+          
+          - Il livello di difficoltà dell'allenamento e la durata desiderata in minuti.
+          - Alcuni allenamenti recenti, così che l'allenamento generato tenga conto del pregresso dell'utente che alleni.
+          - L'attrezzatura che puoi utilizzare per creare l'allenamento.
+          - Opzionalmente, alcune note scritte dall'utente con richieste specifiche o preferenze riguardo all'allenamento.
+        `,
+      },
+      {
+        role: 'user',
+        content: `
+          Genera un allenamento per oggi: ${format(new Date(), 'EEEE dd LLLL yyyy', { locale: it })} basato sui miei dati storici.
+
+          DATI UTENTE:
+          - Livello: ${trainingLevel}
+          - Durata desiderata: ${desiredDuration} minuti
+          - Note aggiuntive o preferenze: ${notes ? notes : 'Nessuna nota fornita dall utente.'}
+
+          DATABASE ESERCIZI:
+          ${JSON.stringify(exercises)}
+
+          ATTREZZATURA:
+          ${equipments.join(',')}
+
+          ALLENAMENTI RECENTI:
+          ${lastTrainings.map((training) => training.training).join('\n\n')}
+
+          Crea un allenamento completo composto da:
+
+          - Una fase di riscaldamento, opzionale
+          - Una parte principale composta da una serie di circuiti 
+          - Una fase di stretching finale, opzionale
+          - Una breve descrizione dell'allenamento composta al massimo da una frase, nello stile del Sgt Hartman di Full Metal jacket, opzionale.
+
+          Per i circuiti indica serie, ripetizioni (o tempo di esecuzione in secondi se l'esercizio non prevede ripetizioni), ed eventuali tempi di recupero per ogni esercizio.
+          Ometti le parti indicate come opzionali se non sono funzionali all'allenamento.
+          L'allenamento deve variare e non deve ripetere esattamente gli allenamenti precedenti: dev'essere studiato per aiutarmi a migliorare le mie prestazioni sulla base degli ultimi allenamenti.
+          L'allenamento deve rispettare il seguente formato Markdown:
+
+         **Allenamento [data in in italiano in formato EEEE dd LLLL yyyy]**
+          - [Riscaldamento] (opzionale) (link a un video tutorial, opzionale)
+          - **C1x[N]**: [ripetizioni o tempo in secondi] [Esercizio 1], [ripetizioni o tempo in secondi] [Esercizio 2], [ripetizioni o tempo in secondi] [Esercizio N]
+          - **C2x[N]**: [ripetizioni o tempo in secondi] [Esercizio 1], [ripetizioni o tempo in secondi] [Esercizio 2], [ripetizioni o tempo in secondi] [Esercizio N]
+          - **CNx[N]**: [ripetizioni o tempo in secondi] [Esercizio 1], [ripetizioni o tempo in secondi] [Esercizio 2], [ripetizioni o tempo in secondi] [Esercizio N]
+          - [Stretching] (opzionale)
+
+          ### Descrizione (opzionale)
+          - [Dettaglio descrizione]
+
+          Non aggiungere note o spiegazioni aggiuntive.
+        `,
+      },
+    ],
+    model,
+  });
+
+  if (!completion.choices[0]?.message?.content) {
+    throw new Error('Cannot generate a training');
+  }
+
+  return {
+    date: format(new Date(), 'yyyy-MM-dd'),
+    training: completion.choices[0]?.message?.content,
+  };
+}
